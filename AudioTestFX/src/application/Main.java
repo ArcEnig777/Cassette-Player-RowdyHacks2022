497d0d7362129c41d51e1008e5394e6ddaa717d6
wrinklykong <61801972+wrinklykong@users.noreply.github.com>
2022-03-26 13:34:43 -0500

    First commit thing idk
    Importing in files, playing files, buttons, ugly display.

---
+++ AudioTestFX/src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+	
+import java.io.File;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			URL url = new File("src/application/view/MainView.fxml").toURI().toURL();
+			AnchorPane root = (AnchorPane)FXMLLoader.load(url);
+			Scene scene = new Scene(root,800,800);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
